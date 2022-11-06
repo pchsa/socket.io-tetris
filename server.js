f393a69c355ad6c5de93c29ecb120680cc884efd
psarm1 <40935723+psarm1@users.noreply.github.com>
2022-11-06 18:28:44 +1000

    modified server.js

--- server.js
+++ server.js
@@ -106,6 +106,6 @@ io.on('connection', socket => {
 })
 
 // Run server on port
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
